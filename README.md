18c2d92376584d7e095200c01af5c932354b82dc
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-10 16:13:42 +0900

    error

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # youtubeClone
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch ->
+/videos/edit ->
+/videos/delete ->
+/videos/comments
+/videos/comments/delete
--- src/server.js
+++ src/server.js
@@ -1,36 +1,19 @@
 import express from "express";
-const app = express();
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("ALLOWED");
-  next();
-};
-
+const app = express();
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", (req, res) => {
-  return res.send("HELLO");
-});
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-app.get("/login", (req, res) => {
-  return res.send("Login Page");
-});
-
-app.get("/protected", (req, res) => {
-  return res.send("Welcome to the private rounge");
-});
-
-app.listen(PORT, () =>
-  console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`)
-);
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+app.listen(PORT, handleListening);
