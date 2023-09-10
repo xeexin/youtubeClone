43a40090588a8010d0a388b20ae75cd4be69a3aa
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-11 01:30:23 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,13 @@
 //global
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("search");
 
 //video
 export const see = (req, res) => {
-  res.send(`Watch video #${req.params.id}`);
+  res.render("watch", { pageTitle: "Watch Video" });
 };
 export const edit = (req, res) => {
-  res.send("Edit Video");
+  res.render("edit", { pageTitle: "Edit Video" });
 };
 export const deleteVideo = (req, res) => {
   res.send("Delete Video");
