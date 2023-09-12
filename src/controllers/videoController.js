271cc912515e3ba8b49de00c6bf2638ee338de5c
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-12 15:33:04 +0900

    before db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,57 @@
+let videos = [
+  {
+    title: "#1",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min age",
+    view: 1,
+    id: 1,
+  },
+  {
+    title: "#2",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min age",
+    view: 25,
+    id: 2,
+  },
+  {
+    title: "#3",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min age",
+    view: 11,
+    id: 3,
+  },
+  {
+    title: "#4",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 min age",
+    view: 35,
+    id: 4,
+  },
+];
+
 //global
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.send("search");
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", videos });
+};
 
 //video
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch Video" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => {
-  res.render("edit", { pageTitle: "Edit Video" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
 };
-
-export const upload = (req, res) => res.send("Upload Video");
