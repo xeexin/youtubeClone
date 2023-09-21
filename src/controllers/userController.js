5d8f4dbcd0398587cecf07c3c08582f6ad953043
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-22 01:43:01 +0900

    login part 1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
+
 //global
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Create Account" });
@@ -13,22 +15,49 @@ export const postJoin = async (req, res) => {
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.render("join", {
+    return res.status(400).render("join", {
       pageTitle: "Create Account",
       errorMessage: "Username/Email is already taken",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Create Account",
+      errorMessage: error._message,
+    });
+  }
 };
-export const login = (req, res) => res.send("login");
+
+export const getLogin = (req, res) => {
+  res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Username doesn't exist",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Wrong password",
+    });
+  }
+  return res.redirect("/");
+};
 
 //user
 export const edit = (req, res) => res.send("Edit User");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -34,7 +34,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video Not Found" });
+    return res.status(404).render("404", { pageTitle: "Video Not Found" });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -59,7 +59,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
