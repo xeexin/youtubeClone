98d9cf45bfd207b262040435383d0dfc16e8964b
LIM HEE JIN <xeexin0331@gmail.com>
2023-09-23 01:30:14 +0900

    login part 2

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
+import { error } from "console";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +15,15 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "Hello",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
