8ce0d69b56735f7774073ff25eb2db37e1a264ec
LIM HEE JIN <xeexin0331@gmail.com>
2023-10-09 01:30:40 +0900

    CSS Part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,7 +5,9 @@ import User from "../models/User";
 //global
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -114,9 +116,9 @@ export const search = async (req, res) => {
   if (keyword) {
     videos = await Video.find({
       title: {
-        $regex: new RegExp(keyword, "i"),
+        $regex: new RegExp(`${keyword}$`, "i"),
       },
-    });
+    }).populate("owner");
   }
   return res.render("search", { pageTitle: `Search`, videos });
 };
