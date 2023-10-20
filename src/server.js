1316cebc8750b77515fbf4ab2c192a3c041cf9aa
LIM HEE JIN <xeexin0331@gmail.com>
2023-10-20 15:28:01 +0900

    resister view controller

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import { localsMiddleware } from "./middlewares";
+import apiRouter from "./routers/apiRouter";
+
 const app = express();
 const logger = morgan("dev");
 app.set("view engine", "pug");
@@ -28,4 +30,5 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/user", userRouter);
+app.use("/api", apiRouter);
 export default app;
