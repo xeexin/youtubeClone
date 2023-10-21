cef71881b3bfb0e9bbf7d340d6ee1424de141748
LIM HEE JIN <xeexin0331@gmail.com>
2023-10-22 01:07:34 +0900

    thumbnail upload pt1

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,6 +4,7 @@ import { type } from "os";
 const videoSchema = new mongoose.Schema({
   title: { type: String, requred: true, trim: true, maxLength: 20 },
   fileUrl: { type: String, require: true },
+  thumbUrl: { type: String, required: true },
   description: { type: String, requred: true, trim: true, maxLength: 50 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
--- src/server.js
+++ src/server.js
@@ -23,6 +23,11 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
 
 app.use(localsMiddleware);
 app.use("/uploads", express.static("uploads"));
